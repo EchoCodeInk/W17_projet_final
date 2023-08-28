aaa42eb7ac5a422602db4897ba893a38fe1bbbc1
Anto <Tonydude007@gmail.com>
2023-08-28 11:44:37 -0400

    ActionBarCustom
    ...

--- application_Mobile/app/src/main/java/com/example/w17_application/MainActivity.java
+++ application_Mobile/app/src/main/java/com/example/w17_application/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.w17_application;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // ACTION BAR
+        View customActionBar = getLayoutInflater().inflate(R.layout.custom_action_bar, null);
+        ActionBar.LayoutParams layoutParams = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(customActionBar, layoutParams);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        ImageView cart = customActionBar.findViewById(R.id.CartImg);
+        Drawable cartDrawable = ContextCompat.getDrawable(this, R.drawable.yellow_shopping_cart);
+        cart.setImageDrawable(cartDrawable);
+        ImageView profile = customActionBar.findViewById(R.id.ProfilePic);
+        Drawable profileDrawable = ContextCompat.getDrawable(this, R.drawable.no_user);
+        profile.setImageDrawable(profileDrawable);
     }
 }
