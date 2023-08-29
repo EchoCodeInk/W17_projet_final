53588eec59f872e5793f64b6bca9d6f24b63117b
Anto <Tonydude007@gmail.com>
2023-08-28 21:17:40 -0400

    Nav Bar et footer
    .

--- application_Mobile/app/src/main/java/com/example/w17_application/MainActivity.java
+++ application_Mobile/app/src/main/java/com/example/w17_application/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
@@ -24,13 +25,33 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(customActionBar, layoutParams);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         ImageView cart = customActionBar.findViewById(R.id.CartImg);
         Drawable cartDrawable = ContextCompat.getDrawable(this, R.drawable.yellow_shopping_cart);
         cart.setImageDrawable(cartDrawable);
+        cart.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, CartActivity.class);
+            startActivity(intent);
+        });
+
         ImageView profile = customActionBar.findViewById(R.id.ProfilePic);
         Drawable profileDrawable = ContextCompat.getDrawable(this, R.drawable.no_user);
         profile.setImageDrawable(profileDrawable);
+        profile.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+            startActivity(intent);
+        });
+
         TextView title = customActionBar.findViewById(R.id.TitleOfPage);
         title.setText("The Sac Team - Home");
+
+        //NAV BAR
+        ImageView burgerMenu = findViewById(R.id.BurgerMenu);
+        Drawable burgerMenuDrawable = ContextCompat.getDrawable(this, R.drawable.burger_menu_light);
+        burgerMenu.setImageDrawable(burgerMenuDrawable);
+
+        ImageView searchImg = findViewById(R.id.imgSearch);
+        Drawable searchImgDrawable = ContextCompat.getDrawable(this, R.drawable.search);
+        searchImg.setImageDrawable(searchImgDrawable);
     }
 }
