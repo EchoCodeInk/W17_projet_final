6f5f63bcccf714745b8840a10f271d8f490c682b
Anto <Tonydude007@gmail.com>
2023-08-29 11:15:01 -0400

    modif page product - Mobile

--- application_Mobile/app/src/main/java/com/example/w17_application/ProductActivity.java
+++ application_Mobile/app/src/main/java/com/example/w17_application/ProductActivity.java
@@ -1,11 +1,18 @@
 package com.example.w17_application;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.w17_application.adapter.ProductAdapter;
 import com.example.w17_application.entite.Product;
@@ -21,6 +28,33 @@ public class ProductActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+
+        // ACTION BAR
+        View customActionBar = getLayoutInflater().inflate(R.layout.custom_action_bar, null);
+        ActionBar.LayoutParams layoutParams = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(customActionBar, layoutParams);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        ImageView cart = customActionBar.findViewById(R.id.CartImg);
+        Drawable cartDrawable = ContextCompat.getDrawable(this, R.drawable.yellow_shopping_cart);
+        cart.setImageDrawable(cartDrawable);
+        cart.setOnClickListener(v -> {
+            Intent intent = new Intent(ProductActivity.this, CartActivity.class);
+            startActivity(intent);
+        });
+
+        ImageView profile = customActionBar.findViewById(R.id.ProfilePic);
+        Drawable profileDrawable = ContextCompat.getDrawable(this, R.drawable.no_user);
+        profile.setImageDrawable(profileDrawable);
+        profile.setOnClickListener(v -> {
+            Intent intent = new Intent(ProductActivity.this, AccountActivity.class);
+            startActivity(intent);
+        });
+
+        TextView title = customActionBar.findViewById(R.id.TitleOfPage);
+        title.setText("The Sac Team - Products");
+
         context = this;
         gridViewMain = findViewById(R.id.list_view_products);
         ArrayList<Product> products = ProductManager.getAll(this);
