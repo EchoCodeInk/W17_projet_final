49f26cc8be2ed70f614b8796cf77a11560b1c583
Taoufik Boussemousse <114195598+BoussemousseT@users.noreply.github.com>
2023-08-30 12:58:55 -0400

    recuperation d'informations sur page payment

--- application_Mobile/app/src/main/java/com/example/w17_application/PaymentActivity.java
+++ application_Mobile/app/src/main/java/com/example/w17_application/PaymentActivity.java
@@ -2,7 +2,12 @@ package com.example.w17_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.w17_application.entite.Product;
+import com.example.w17_application.manager.ProductManager;
 
 public class PaymentActivity extends AppCompatActivity {
 
@@ -10,6 +15,12 @@ public class PaymentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        Intent intent = getIntent();
+        int idValue = intent.getIntExtra("id", -1);
+        String quantityValue = intent.getStringExtra("quantity");
+
+        Toast.makeText(this, idValue + " + " + quantityValue, Toast.LENGTH_SHORT).show();
+//        Product product = ProductManager.getById(this, idValue);
 
     }
 }
