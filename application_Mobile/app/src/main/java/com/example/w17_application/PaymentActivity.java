430d104ba2fc64facbf650afbb269e05362f0bd3
Taoufik Boussemousse <114195598+BoussemousseT@users.noreply.github.com>
2023-08-30 12:40:33 -0400

    page payment

---
+++ application_Mobile/app/src/main/java/com/example/w17_application/PaymentActivity.java
@@ -0,0 +1,15 @@
+package com.example.w17_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+    }
+}
